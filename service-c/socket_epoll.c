7df40c98e22ca5b708a0803df57cf787e3794c48
xwp <xwp_fullstack@163.com>
2017-07-21 17:28:57 +0800

    creat socket_epoll.c and .h file

---
+++ service-c/socket_epoll.c
@@ -0,0 +1,10 @@
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <stdio.h>
+
---
+++ service-c/socket_epoll.h
@@ -0,0 +1,6 @@
+#ifndef poll_socket_epoll_h
+#define poll_socket_epoll_h
+
+
+
+#endif
